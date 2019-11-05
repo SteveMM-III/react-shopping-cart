ac476ebe1e1cbbeefef5400a28a1a6fcc5246946
Kronykus <MoreheadSteve@gmail.com>
2019-11-05 16:46:37 -0600

    formatting

--- src/components/ShoppingCartItem.jsx
+++ src/components/ShoppingCartItem.jsx
@@ -13,11 +13,11 @@ const Item = props => {
 
 	return (
 		<div className="shopping-cart_item">
-			<img src={props.image} alt={`${props.title} book`} />
+			<img src={ props.image } alt={`${ props.title } book`} />
 
 			<div>
-				<h1>{props.title}</h1>
-				<p>$ {props.price}</p>
+				<h1>{ props.title }</h1>
+				<p>${ props.price }</p>
 				<button onClick={ removeItem }>Remove from cart</button>
 			</div>
 		</div>
